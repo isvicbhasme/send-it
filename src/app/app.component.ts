2aa06018c282b37de5dbf26ce60a6fbf2794fb8c
Isaac Bhasme <isvicbhasme@gmail.com>
2019-09-02 00:10:03 +0530

    Basic login-page with oauth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms'
+
+import * as firebase from 'firebase/app';
+import * as firebaseui from 'firebaseui'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ui: firebaseui.auth.AuthUI;
+
   title = 'send-it';
+  signup: boolean = false;
+  signupForm = new FormGroup({
+    signupEmail : new FormControl('', Validators.email),
+    signupPassword : new FormControl('', Validators.min(5))
+  });
+
+  constructor(private afAuth: AngularFireAuth) {
+  }
+
+  ngOnInit(): void {
+    let uiConfig = {
+      signInSuccessUrl: '<url-to-redirect-to-on-success>',
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      ],
+      // tosUrl and privacyPolicyUrl accept either url string or a callback
+      // function.
+      // Terms of service url/callback.
+      tosUrl: '<your-tos-url>',
+      // Privacy policy url/callback.
+      privacyPolicyUrl: function() {
+        window.location.assign('<your-privacy-policy-url>');
+      }
+    };
+    this.ui = new firebaseui.auth.AuthUI(this.afAuth.auth);
+    this.ui.start('#firebaseui-auth-container', uiConfig);
+  }
+
+  signupFormSubmit() {
+    console.log(this.signupForm.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,9 +16,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [
+    AngularFireAuth,
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
